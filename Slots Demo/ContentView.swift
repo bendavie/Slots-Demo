fa1bdb153b75c217841c54b905c112bd3abf6794
bendavie <benjamindavie6@gmail.com>
2020-08-25 11:29:12 +0100

    Initial Commit

---
+++ Slots Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Slots Demo
+//
+//  Created by Benjamin Miles Davie on 25/08/2020.
+//  Copyright © 2020 Benjamin Miles Davie. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
